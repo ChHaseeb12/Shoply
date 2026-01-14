5143157b8dbfb4fa6b222ce20b7d78a0f3ec74d4
Haseeb <haseebarshad90wb@gmail.com>
2026-01-14 19:53:33 +0500

    Dashboard Screen 1 (WIP)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoply/screens/dashboard/dashboard_screen_1.dart';
 import 'package:shoply/screens/splash/splash_screen.dart';
 
 void main() {
@@ -10,6 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: SplashScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: DashboardScreen1());
   }
 }
--- lib/screens/dashboard/dashboard_screen_1.dart
+++ lib/screens/dashboard/dashboard_screen_1.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:shoply/components/grid_painter.dart';
 import 'package:shoply/components/subtle_radial_glow.dart';
 import 'package:shoply/config/constants/app_colors.dart';
+import 'package:shoply/config/constants/text_styles.dart';
 
 class DashboardScreen1 extends StatefulWidget {
   const DashboardScreen1({super.key});
@@ -11,50 +13,65 @@ class DashboardScreen1 extends StatefulWidget {
 }
 
 class _DashboardScreen1State extends State<DashboardScreen1> {
+
+ String assetName = 'assets/icons/compass_logo.svg';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          // Grid Pattren Background
-          Positioned.fill(child: CustomPaint(painter: GridPainter())),
-
-          // Subtle Radial Glow
-          SubtleRadialGlow(),
-
-          // Centered Icon and Text
-          Center(
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                // Outer circle (lightest)
-                Container(
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.primary.withOpacity(0.08),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            // Grid Pattren Background
+            Positioned.fill(child: CustomPaint(painter: GridPainter())),
+        
+            // Subtle Radial Glow
+            SubtleRadialGlow(),
+        
+            // Centered Icon and Text
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // Icon
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      // Outer circle (lightest)
+                      Container(
+                        width: 160,
+                        height: 160,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.primary.withOpacity(0.08),
+                        ),
+                      ),
+                      // Inner circle
+                      Container(
+                        width: 115,
+                        height: 115,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.primary.withOpacity(0.15),
+                        ),
+                      ),
+                      // Icon
+                      SvgPicture.asset(assetName, width: 60, height: 60, colorFilter: ColorFilter.mode(AppColors.primary, BlendMode.srcIn,))
+                    ],
                   ),
-                ),
-                // Inner circle
-                Container(
-                  width: 120,
-                  height: 120,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: AppColors.primary.withOpacity(0.15),
-                  ),
-                ),
-                // Icon (compass/navigation style)
-                Icon(
-                  Icons.navigation_outlined, // or Icons.explore_outlined
-                  size: 50,
-                  color: AppColors.primary,
-                ),
-              ],
+                  SizedBox(height: 48,),
+                  // Text
+                  Text('Curated for you', style: TextStyle(color: AppColors.textHint, fontSize: 16, fontWeight: FontWeight.w500),),
+                  SizedBox(height: 15,),
+                  Text('Discover', style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold)),
+                  SizedBox(height: 15,),
+                  Text('Explore thousands of products handpicked\n by our experts. Find exactly what you need.', style: TextStyle(color: AppColors.textSecondary, fontSize: 17, fontWeight: FontWeight.normal))
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
